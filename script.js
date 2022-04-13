5901153a3cfb58b0a559e5521850f3b66482c934
2ood <kyungmin.official0@gmail.com>
2022-04-13 15:53:28 +0900

    script : implement firebase

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD6VF0W-wEnSyp5IxtPSEhdzicC-9yKjSU",
+  authDomain: "ood-plants.firebaseapp.com",
+  projectId: "ood-plants",
+  storageBucket: "ood-plants.appspot.com",
+  messagingSenderId: "1017909141385",
+  appId: "1:1017909141385:web:5a3e7787e45b8ae4bd4b58"
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
+const fs = app.firestore();
