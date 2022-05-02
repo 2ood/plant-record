3d0e6f029a4566cfae47b110945b61e98ed47165
2ood <kyungmin.official0@gmail.com>
2022-05-03 01:04:02 +0900

    bugfix

--- script/post.js
+++ script/post.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(window.location.search);
 
 if(params.has("id")) {
   console.log(`loading ${params.get("id")}.md`);
-  fetch(`/posts/${params.get("id")}.html`).then((res)=>{
+  fetch(`posts/${params.get("id")}.html`).then((res)=>{
       document.querySelector("maintext").innerHTML=res.text();
   });
 }
