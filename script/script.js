f7e3aaa3f334b9da51e2e72c20b2a3d7082ab6ff
2ood <kyungmin.official0@gmail.com>
2022-07-16 02:51:10 +0900

    design : set designs for desktop page

--- script/script.js
+++ script/script.js
@@ -1,86 +1,32 @@
-const left_button = document.getElementById("left");
-const right_button = document.getElementById("right");
+const params = new URLSearchParams(window.location.search);
 
-const horizontal = document.querySelector("horizontal");
-const columns = horizontal.getElementsByTagName("column");
-const progressbar = document.querySelector("progressbar");
-const progress_spot = progressbar.querySelector("spot");
-const posts = document.querySelectorAll("ul.posts > li");
-
-let timer =null;
-
-//add event listeners of horizontal touch scrolls
-horizontal.addEventListener("touchstart",()=>{
-  if(timer!=null) clearTimeout(timer);
-});
-
-horizontal.addEventListener("touchend",()=>{
-    timer = setTimeout(function () {
-        const index = activeColumnIndex();
-        const next = focusedColumnIndex();
-        if(index!=next) moveActiveClass(index,next);
-        clearTimeout(timer);
-    }, 1500);
-});
-
-//add event listeners of horizontal scroll buttons
-left_button.addEventListener("click", ()=>{
-    horizontal.scrollLeft-=horizontal.offsetWidth;
-
-    const index = activeColumnIndex();
-    if(index-1>=0) moveActiveClass(index,index-1);
-});
-
-right_button.addEventListener("click", ()=>{
-  horizontal.scrollLeft+=horizontal.offsetWidth;
-
-  const index = activeColumnIndex();
-  if(index+1<columns.length) moveActiveClass(index,index+1);
-});
-
-//add onclick event listeners to post lists
-for(let p of posts) {
-  p.addEventListener("click",(evt)=>{
-    let target =evt.srcElement;
-    while(true) {
-      console.log(`checking`, target);
-      if(target.tagName=="LI" && target.parentNode.classList.contains("posts")) break;
-      else {target = target.parentNode;}
-    }
-    window.location.href=`post.html?id=${target.id}`;
-  });
-}
-
-
-
-function moveActiveColumn(index, next) {
-  columns[index].classList.remove("active");
-  if(next>=0 && next<columns.length) columns[next].classList.add("active");
-}
-
-function moveProgressBar(index,next,total) {
-  progress_spot.style.marginLeft =`${(index*100.0)/total}%`; //initialize
-  progressbar.classList.remove("vanish");
-
-  if(next>=0 &&next<=total)progress_spot.style.marginLeft =`${(next*100.0)/total}%`;
-  setTimeout(()=>{progressbar.classList.add("vanish");},500);
-}
-
-function moveActiveClass(from, to){
-  moveActiveColumn(from,to);
-  moveProgressBar(from,to,columns.length);
-}
-
-function activeColumnIndex() {
-  let index = 0;
-  for(;index<columns.length;index++) {
-    if(columns[index].classList.contains("active")) return index;
+window.addEventListener("load",()=>{
+  if(params.has("plant")) {
+    const plant_name=params.get("plant");
+    console.log(`loading ${plant_name}`);
+    loadPlant(plant_name);
   }
-  return index;
+  console.log("hey!");
+  const sidebar_li  = document.querySelectorAll("nav li");
+  sidebar_li.forEach((li)=>{
+    li.addEventListener("click",(evt)=>{
+      const plant_name = li.innerHTML;
+      const new_url = `${window.location.href.split('?')[0]}?plant=${plant_name}`;
+      updateURL(new_url);
+      loadPlant(plant_name);
+    });
+  });
+});
+
+function loadPlant(plantName) {
+  const content = document.getElementById("content");
+  content.textContent = plantName;
 }
 
-function focusedColumnIndex() {
-  const x = horizontal.scrollLeft;
-  const viewport_width = window.innerWidth;
-  return (x/viewport_width);
+function updateURL(url){
+  const nextURL = url;
+  const nextTitle = 'plant view';
+  const nextState = { additionalInformation: 'Updated the URL with JS' };
+
+  window.history.pushState(nextState, nextTitle, nextURL);
 }
