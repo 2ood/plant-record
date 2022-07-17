355eb46ffec70fc0499b13257ee8315548ce087c
2ood <kyungmin.official0@gmail.com>
2022-07-17 23:39:22 +0900

    bugfix : typo

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const app = express();
 const plantRouter = require('./router/plantRouter.js');
 
 app.use('/',express.static(__dirname));
-app.use('/img',express.static(__dirname)+"/img");
-app.use('/mobile',express.static(__dirname)+"/mobile");
-app.use('/plant-profile',express.static(__dirname)+"/plant-profile");
-app.use('/posts',express.static(__dirname)+"/posts");
-app.use('/router',express.static(__dirname)+"/router");
-app.use('/script',express.static(__dirname)+"/script");
-app.use('/style',express.static(__dirname)+"/style");
+app.use('/img',express.static(__dirname+"/img"));
+app.use('/mobile',express.static(__dirname+"/mobile"));
+app.use('/plant-profile',express.static(__dirname+"/plant-profile"));
+app.use('/posts',express.static(__dirname+"/posts"));
+app.use('/router',express.static(__dirname+"/router"));
+app.use('/script',express.static(__dirname+"/script"));
+app.use('/style',express.static(__dirname+"/style"));
 
 app.use('/profile',plantRouter);
 
