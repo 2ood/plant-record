5dc8a6c351c90b7daecf77833b1851918f0d1408
2ood <kyungmin.official0@gmail.com>
2022-07-18 16:20:42 +0900

    feat : made it fit to mobile screen too.

--- script/script.js
+++ script/script.js
@@ -1,21 +1,13 @@
 const params = new URLSearchParams(window.location.search);
 
 window.addEventListener("load",async ()=>{
-  if(params.has("plant")) {
-    const plant_name=params.get("plant").toLowerCase();
-    console.log(`loading ${plant_name}`);
-    const rawJson = await loadPlantJson(plant_name);
-    updatePlantProfileDOM(rawJson);
-  }
 
   const sidebar_li  = document.querySelectorAll("nav li");
   sidebar_li.forEach((li)=>{
     li.addEventListener("click",async (evt)=>{
+      document.getElementById("nav").classList.remove("show");
       const plant_name = li.innerHTML.toLowerCase();
-      const new_url = `${window.location.href.split('?')[0]}?plant=${plant_name}`;
-      updateURL(new_url);
-      const rawJson = await loadPlantJson(plant_name);
-      updatePlantProfileDOM(rawJson);
+      showPlantProfile(plant_name);
     });
   });
 
@@ -25,8 +17,35 @@ window.addEventListener("load",async ()=>{
 
   //svg.querySelector("#week-4 .fri").style.fill="coral";
 
+  const hamburger = document.getElementById("open-nav");
+  hamburger.addEventListener("click",(evt)=>{
+    const nav = document.getElementById("nav");
+    nav.classList.add("show");
+  });
 
+  const close = document.getElementById("close-nav");
+  close.addEventListener("click",(evt)=>{
+    const nav = document.getElementById("nav");
+    nav.classList.remove("show");
+  });
+
+  if(params.has("plant")) {
+    const plant_name=params.get("plant").toLowerCase();
+    console.log(`loading ${plant_name}`);
+    showPlantProfile(plant_name);
+  }
+  else {
+    showPlantProfile("apple");
+  }
 });
+
+async function showPlantProfile(plantName) {
+
+  const new_url = `${window.location.href.split('?')[0]}?plant=${plantName}`;
+  updateURL(new_url);
+  const rawJson = await loadPlantJson(plantName);
+  updatePlantProfileDOM(rawJson);
+}
 
 async function loadPlantJson(plantName) {
   const api_url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/profile/${plantName}`;
@@ -40,6 +59,7 @@ function updatePlantProfileDOM(json) {
   const title = document.getElementById("title");
   const profile_img = document.getElementById("profile-img");
   const details_pre = document.getElementById("details-pre");
+  const status = document.getElementById("status");
   const achievements_group = document.getElementById("chipgroup-achievements");
   const stats_group = document.getElementById("chipgroup-stats");
   const garden_today_ul = document.querySelector("#gardenings-today ul");
@@ -51,6 +71,10 @@ function updatePlantProfileDOM(json) {
 
   title.textContent = json.title;
   profile_img.src = json.img_src;
+  status.classList = [];
+  status.classList.add(json.status);
+  if(json.status==="dead") profile_img.classList.add("grayscale");
+  else profile_img.classList.remove("grayscale");
   details_pre.textContent = json.details;
   let chips = "";
   json.achievements.forEach((achievement)=>{
@@ -71,4 +95,8 @@ function updateURL(url){
   const nextState = { additionalInformation: 'Updated the URL with JS' };
 
   window.history.pushState(nextState, nextTitle, nextURL);
+}
+
+function onImgError(e){
+  e.src="img/image-to-be-updated.png";
 }
